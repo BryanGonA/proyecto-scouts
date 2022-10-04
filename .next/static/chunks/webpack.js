d5c37919e7444766eb72622dbab950404c336e2e
BryanGonA <bryan.gonzalez_alv@uao.edu.co>
2022-10-03 22:38:47 -0500

    Actualizaciones

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e43a4553e05b0de"; }
+/******/ 		__webpack_require__.h = function() { return "6d01bc329bf718cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
